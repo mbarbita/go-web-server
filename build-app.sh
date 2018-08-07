517c26ee6fea721058f8c9ffd93ed4f8cbb69939
mbarbita <mbarbita@gmail.com>
2018-08-07 07:45:23 +0300

    Linux LF

--- build-app.sh
+++ build-app.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 echo ""
-echo "run: go get -u -v file.go"
-echo "run: go build -v file.go"
+echo "run: go get -d -v"
+echo "run: go build -v"
 docker run -it --rm -v "$PWD":/usr/src/go-server -w /usr/src/go-server golang /bin/bash
